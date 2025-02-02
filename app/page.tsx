6100eca467cc4df022f84f4c378ae1e04498c186
ΛΑΖΑΡΟΣ ΖΗΣΚΟΣ <ziskoslaz@gmail.com>
2025-02-02 20:05:21 +0200

    finished project

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,13 @@ import RecentProjects from "@/components/RecentProjects";
 import { navItems } from "@/data";
 import Experience from "@/components/Experience";
 import Approach from "@/components/Approach";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <main
       className="relative bg-black-100 flex justify-center items-center flex-col
-    overflow-hidden mx-auto sm:px-10 px-5"
+    mx-auto sm:px-10 px-5 overflow-clip"
     >
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
@@ -19,6 +20,7 @@ export default function Home() {
         <RecentProjects />
         <Experience />
         <Approach />
+        <Footer />
       </div>
     </main>
   );
